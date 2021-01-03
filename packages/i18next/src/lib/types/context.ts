139c10bdcd7d2ca7c2b40d4cd59a76fb6d3ca69d
Nytelife26 <xtylerjrx@gmail.com>
2021-01-03 19:41:34 +0000

    docs(i18next): document context interfaces

--- packages/i18next/src/lib/types/context.ts
+++ packages/i18next/src/lib/types/context.ts
@@ -1,5 +1,19 @@
+/**
+ * Context for the guild, used in fetchLanguage functions.
+ * Should be an extension of your framework's Guild object, or the corresponding I18n extension.
+ */
 export interface I18nGuildContext {}
+
+/**
+ * Context for the channel, used in fetchLanguage functions.
+ * Should be an extension of your framework's Channel object, or the corresponding I18n extension.
+ */
 export interface I18nChannelContext {}
+
+/**
+ * Context for the user sending a message, used in fetchLanguage functions.
+ * Should be an extension of your framework's User object, or the corresponding I18n extension.
+ */
 export interface I18nAuthorContext {}
 
 /**
