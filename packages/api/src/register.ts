28ef5a71474a2c57611bc62674477593ae276676
Jeroen Claassens <support@favware.tech>
2021-02-03 17:38:32 +0100

    chore: hoo boy

--- packages/api/src/register.ts
+++ packages/api/src/register.ts
@@ -1,10 +1,8 @@
+import './index';
 import { Plugin, postInitialization, preLogin, SapphireClient } from '@sapphire/framework';
 import type { ClientOptions } from 'discord.js';
 import { join } from 'path';
-import { Server, ServerOptions } from './lib/structures/http/Server';
-import type { MediaParserStore } from './lib/structures/MediaParserStore';
-import type { MiddlewareStore } from './lib/structures/MiddlewareStore';
-import type { RouteStore } from './lib/structures/RouteStore';
+import { Server } from './lib/structures/http/Server';
 
 /**
  * @since 1.0.0
@@ -31,24 +29,6 @@ export class Api extends Plugin {
 	 */
 	public static async [preLogin](this: SapphireClient): Promise<void> {
 		await this.server.connect();
-	}
-}
-
-declare module 'discord.js' {
-	export interface Client {
-		server: Server;
-	}
-
-	export interface ClientOptions {
-		api?: ServerOptions;
-	}
-}
-
-declare module '@sapphire/framework' {
-	interface StoreRegistryEntries {
-		routes: RouteStore;
-		mediaParsers: MediaParserStore;
-		middlewares: MiddlewareStore;
 	}
 }
 
