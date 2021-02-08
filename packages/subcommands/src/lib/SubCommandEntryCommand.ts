9725a0351f00d4e70ed262701b5753c03734f78d
Soumil <soumildiscord@gmail.com>
2021-02-08 23:19:58 +0530

    fix: fix SubCommandEntryCommand

--- packages/subcommands/src/lib/SubCommandEntryCommand.ts
+++ packages/subcommands/src/lib/SubCommandEntryCommand.ts
@@ -1,8 +1,9 @@
+import { isFunction } from '@sapphire/utilities';
 import { SubCommandEntry } from './SubCommandEntry';
 
 export class SubCommandEntryCommand extends SubCommandEntry {
-	public run(context: SubCommandEntry.RunContext): unknown {
-		const command = context.message.client.stores.get('commands').get(this.input);
+	public async run(context: SubCommandEntry.RunContext): Promise<unknown> {
+		const command = context.message.client.stores.get('commands').get(isFunction(this.input) ? await this.input(context) : this.input);
 		if (command) return command.run(context.message, context.args, context.context);
 		throw new ReferenceError(`The command '${this.input}' does not exist.`);
 	}
