bedb8f091345cf297fe1d899bfebd891142af7ea
Feralheart <feralheart@feralheart.dev>
2021-12-30 21:59:55 +0100

    fix(pattern-commands): No more dual-trigger

--- packages/pattern-commands/src/index.ts
+++ packages/pattern-commands/src/index.ts
@@ -4,7 +4,9 @@ export * from './lib/structures/PaternCommandStore';
 export * from './lib/structures/PatternCommand';
 export * from './lib/utils/PaternCommandEvents';
 export * from './lib/utils/PatternCommandInterfaces';
-export * as PatternCommandListeners from './listeners';
+export { CommandAcceptedListener as PluginPatternCommandsCommandAcceptedListener } from './listeners/PluginCommandAccepted';
+export { MessageParseListener as PluginPatternCommandsMessageParseListener } from './listeners/PluginMessageParse';
+export { PreCommandRunListener as PluginPatternCommandsPreCommandRunListener } from './listeners/PluginPreCommandRun';
 
 declare module '@sapphire/pieces' {
 	interface StoreRegistryEntries {
--- packages/pattern-commands/src/listeners/index.ts
+++
@@ -1,3 +0,0 @@
-export { CommandAcceptedListener as PluginPatternCommandsCommandAcceptedListener } from './PluginCommandAccepted';
-export { MessageParseListener as PluginPatternCommandsMessageParseListener } from './PluginMessageParse';
-export { PreCommandRunListener as PluginPatternCommandsPreCommandRunListener } from './PluginPreCommandRun';
