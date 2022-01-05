fc90071f3d6fe97a6e83d0daa995b15a863b8072
Just a Badly Drawn TABS Dude <45307955+Suyashtnt@users.noreply.github.com>
2022-01-05 12:03:43 +0200

    hmr: WIP refactor way it checks for reloads

--- packages/hmr/src/register.ts
+++ packages/hmr/src/register.ts
@@ -1,6 +1,5 @@
-import { ILogger, Piece, Plugin, postInitialization, SapphireClient, Store } from '@sapphire/framework';
+import { Plugin, postInitialization, SapphireClient, Store } from '@sapphire/framework';
 import chokidar from 'chokidar';
-import { basename, join } from 'path';
 
 /**
  * @since 1.0.0
@@ -13,49 +12,19 @@ export class HmrPlugin extends Plugin {
 		if (process.env.NODE_ENV === 'development') {
 			this.logger.info('HMR is enabled!');
 
-			const watcher = chokidar.watch('.', {
-				ignoreInitial: true
-			});
+			for (const store of this.stores.values()) {
+				const cb = handlePiecePathUpdate.bind(null, store);
 
-			watcher.on('change', (path, _stats) => {
-				const fullPath = join(process.cwd(), path);
-				const commands = this.stores.get('commands');
-				reloadPieceList(commands, this.logger, fullPath);
-
-				const listeners = this.stores.get('listeners');
-				reloadPieceList(listeners, this.logger, fullPath);
-
-				const preconditions = this.stores.get('preconditions');
-				reloadPieceList(preconditions, this.logger, fullPath);
-
-				if (path.endsWith('.js')) {
-					const absolute_path = join(process.cwd(), path);
-					import(absolute_path)
-						.then((module) => {
-							for (const [key, value] of Object.entries(module)) {
-								this.logger.debug(`${key} - ${value}`);
-							}
-						})
-						.catch((err) => this.logger.error(err));
+				for (const path of store.paths) {
+					chokidar.watch(path).on('change', cb);
 				}
-			});
+			}
 		}
 	}
 }
 
-function reloadPieceList<T extends Piece>(list: Store<T>, logger: ILogger, fullLocation: string): void {
-	const piece = list.find((piece) => piece.location.full === fullLocation);
-
-	if (!piece) return;
-
-	piece
-		.reload()
-		.then(() => {
-			logger.info(`Reloaded ${basename(fullLocation)}`);
-		})
-		.catch((err) => {
-			logger.error(err);
-		});
+function handlePiecePathUpdate(store: Store<any>, path: string) {
+	// ...
 }
 
 SapphireClient.plugins.registerPostInitializationHook(HmrPlugin[postInitialization], 'HmrCommands-PostInitialization');
