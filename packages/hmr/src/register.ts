edf7b1478ae0e57989838477f25bbe9e5842ccb1
Just a Badly Drawn TABS Dude <45307955+Suyashtnt@users.noreply.github.com>
2022-01-05 11:30:07 +0200

    hmr: refactor to allow automatic adding of pieces

--- packages/hmr/src/register.ts
+++ packages/hmr/src/register.ts
@@ -1,5 +1,5 @@
-import { Plugin, postInitialization, SapphireClient, Store } from '@sapphire/framework';
-import chokidar from 'chokidar';
+import { Plugin, postLogin, SapphireClient } from '@sapphire/framework';
+import { start } from './index';
 
 /**
  * @since 1.0.0
@@ -8,23 +8,9 @@ export class HmrPlugin extends Plugin {
 	/**
 	 * @since 1.0.0
 	 */
-	public static [postInitialization](this: SapphireClient): void {
-		if (process.env.NODE_ENV === 'development') {
-			this.logger.info('HMR is enabled!');
-
-			for (const store of this.stores.values()) {
-				const cb = handlePiecePathUpdate.bind(null, store);
-
-				for (const path of store.paths) {
-					chokidar.watch(path).on('change', cb);
-				}
-			}
-		}
+	public static [postLogin](this: SapphireClient): void {
+		start();
 	}
 }
 
-function handlePiecePathUpdate(store: Store<any>, path: string) {
-	// ...
-}
-
-SapphireClient.plugins.registerPostInitializationHook(HmrPlugin[postInitialization], 'HmrCommands-PostInitialization');
+SapphireClient.plugins.registerPostLoginHook(HmrPlugin[postLogin], 'Hmr-PostLogin');
