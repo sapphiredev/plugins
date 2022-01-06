f3a21709b27c008fc37b63d319c4aaa7da62589a
Jeroen Claassens <support@favware.tech>
2022-01-06 18:48:54 +0100

    refactor(scheduled-tasks): use namespaced imports

--- packages/scheduled-tasks/src/lib/structures/ScheduledTask.ts
+++ packages/scheduled-tasks/src/lib/structures/ScheduledTask.ts
@@ -1,11 +1,11 @@
-import { Piece, PieceContext, PieceOptions } from '@sapphire/pieces';
+import { Piece } from '@sapphire/pieces';
 import type { Awaitable } from '@sapphire/utilities';
 
 export abstract class ScheduledTask extends Piece {
 	public readonly interval: number | null;
 	public readonly cron: string | null;
 
-	public constructor(context: PieceContext, options: ScheduledTaskOptions) {
+	public constructor(context: Piece.Context, options: ScheduledTaskOptions) {
 		super(context, options);
 		this.interval = options.interval ?? null;
 		this.cron = options.cron ?? null;
@@ -16,7 +16,7 @@ export abstract class ScheduledTask extends Piece {
 
 export interface ScheduledTasks {}
 
-export interface ScheduledTaskOptions extends PieceOptions {
+export interface ScheduledTaskOptions extends Piece.Options {
 	interval?: number | null;
 	cron?: string | null;
 }
