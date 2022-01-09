40107b3ceb7c7720a87d91f06d7635d61e61ee26
Feralheart <feralheart@feralheart.dev>
2022-01-09 12:29:31 +0100

    fix(scheduled-task): SQS type error when using Redis

--- packages/scheduled-tasks/src/register-redis.ts
+++ packages/scheduled-tasks/src/register-redis.ts
@@ -2,3 +2,11 @@ import './index';
 import './register';
 
 export * from './lib/strategies/ScheduledTaskRedisStrategy';
+export type {
+	BullClient,
+	ScheduledTaskRedisStrategy,
+	ScheduledTaskRedisStrategyJob,
+	ScheduledTaskRedisStrategyListOptions,
+	ScheduledTaskRedisStrategyListRepeatedOptions,
+	ScheduledTaskRedisStrategyOptions
+} from './lib/strategies/ScheduledTaskRedisStrategy';
--- packages/scheduled-tasks/src/register-sqs.ts
+++ packages/scheduled-tasks/src/register-sqs.ts
@@ -2,3 +2,4 @@ import './index';
 import './register';
 
 export * from './lib/strategies/ScheduledTaskSQSStrategy';
+export type { ScheduledTaskSQSStrategy, ScheduledTaskSQSStrategyMessageBody, SQSClient } from './lib/strategies/ScheduledTaskSQSStrategy';
