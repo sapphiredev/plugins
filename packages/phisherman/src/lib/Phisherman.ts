0879e15b3bd8c248c30b540622f6999df8056e08
UndiedGamer <hemanthchakka2007@gmail.com>
2022-01-09 17:16:59 +0530

    feat: add jsdoc comments

--- packages/phisherman/src/lib/Phisherman.ts
+++ packages/phisherman/src/lib/Phisherman.ts
@@ -2,11 +2,19 @@ import { fetch, FetchMethods, FetchResultTypes } from '@sapphire/fetch';
 import { fromAsync, isErr } from '@sapphire/framework';
 
 export class Phisherman {
+	/**
+	 * The options for phisherman
+	 */
 	public options;
 	public constructor(options: PhishermanOptions | undefined) {
 		this.options = options;
 	}
 
+	/**
+	 * Check if a link is scam
+	 * @param domain The link to check
+	 * @since 1.0.0
+	 */
 	public async check(domain: string) {
 		const result = await fromAsync(async () => {
 			const check = fetch<PhishermanReturnType>(`https://api.phisherman.gg/v2/domains/check/${domain}`, {
@@ -21,6 +29,11 @@ export class Phisherman {
 		return result.value;
 	}
 
+	/**
+	 * Report a scam domain to phisherman
+	 * @param domain The domain to report
+	 * @since 1.0.0
+	 */
 	public async report(domain: string) {
 		const result = await fromAsync(async () => {
 			const report = await fetch<PhishermanReportType>(
@@ -41,11 +54,6 @@ export class Phisherman {
 	}
 }
 
-/**
- * @typedef {Object} PhishermanOptions
- * @property {string} apiKey - The API key to use for the Phisherman API.
- * @since 1.0.0
- */
 export interface PhishermanOptions {
 	apiKey: string;
 }
