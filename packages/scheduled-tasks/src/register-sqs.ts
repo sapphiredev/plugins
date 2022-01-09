d674845dc5004b5b6820a7cfbe991f6c03b83c02
Jeroen Claassens <support@favware.tech>
2022-01-09 14:50:51 +0100

    chore: speling is difikult
    permalink: http://whatthecommit.com/cd76a053c7f2de2dcd5ad967490bfeb9

--- packages/scheduled-tasks/src/register-sqs.ts
+++ packages/scheduled-tasks/src/register-sqs.ts
@@ -2,4 +2,3 @@ import './index';
 import './register';
 
 export * from './lib/strategies/ScheduledTaskSQSStrategy';
-export type { ScheduledTaskSQSStrategy, ScheduledTaskSQSStrategyMessageBody, SQSClient } from './lib/strategies/ScheduledTaskSQSStrategy';
