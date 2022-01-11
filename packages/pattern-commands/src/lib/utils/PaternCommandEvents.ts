e18509f2df3d3b1fd146e21c6dc199170d671b15
Feralheart <feralheart@feralheart.dev>
2022-01-11 20:16:41 +0100

    fix(scheduled-tasks and pattern-commands): Add params to event typedocs (#222)
    Co-authored-by: Jeroen Claassens <support@favware.tech>

--- packages/pattern-commands/src/lib/utils/PaternCommandEvents.ts
+++ packages/pattern-commands/src/lib/utils/PaternCommandEvents.ts
@@ -1,10 +1,58 @@
+/**
+ * Events emitted during the parsing and command run.
+ * You can use these events for debugging and logging purposes.
+ */
 export enum PatternCommandEvents {
+	/**
+	 * Event that is emitted when the RNG doesn't love the command
+	 * @param message The message where the command was triggered
+	 * @param command The command's piece
+	 * @param alias The alias that triggered the command
+	 */
 	CommandNoLuck = 'patternCommandNoLuck',
+	/**
+	 * Event that is emitted when an alias triggered the command but before parsing the preconditions
+	 * @param payload PatternCommandRunPayload which contains message, command and alias
+	 */
 	PreCommandRun = 'patternCommandPreRun',
+	/**
+	 * Event that is emitted after the preconditions if none of them denied the command
+	 * @param payload PatternCommandAcceptedPayload which contains parameters, context, message, command and alias
+	 */
 	CommandAccepted = 'patternCommandAccepted',
+	/**
+	 * Event that is emitted after the preconditions if at least one of them denied the command
+	 * @param error The error of the precondition which denied the command
+	 * @param payload PatternCommandDeniedPayload which contains parameters, context, message, command and alias
+	 */
 	CommandDenied = 'patternCommandDenied',
+	/**
+	 * Event that is emitted just before the command is ran
+	 * @param message The message where the command was triggered
+	 * @param command The command's piece
+	 * @param alias The alias that triggered the command
+	 */
 	CommandRun = 'patternCommandRun',
+	/**
+	 * Event that is emitted if there's no error while running the command
+	 * @param result The result of command's run
+	 * @param command The command's piece
+	 * @param alias The alias that triggered the command
+	 */
 	CommandSuccess = 'patternCommandSuccess',
+	/**
+	 * Event that is emitted if there's an error while running the command
+	 * @param error The error message which happened while the command was running
+	 * @param command The command's piece
+	 * @param duration The duration which indicates how long it took the command to run
+	 * @param payload PatternCommandAcceptedPayload which contains parameters, context, message, command and alias
+	 */
 	CommandError = 'patternCommandError',
+	/**
+	 * Event that is emitted if the command has finished, regardless of whether an error occurred or not
+	 * @param command The command's piece
+	 * @param duration The duration which indicates how long it took the command to run
+	 * @param payload PatternCommandAcceptedPayload which contains parameters, context, message, command and alias
+	 */
 	CommandFinished = 'patternCommandFinished'
 }
--- packages/scheduled-tasks/src/lib/types/ScheduledTaskEvents.ts
+++ packages/scheduled-tasks/src/lib/types/ScheduledTaskEvents.ts
@@ -3,16 +3,37 @@
  * You can use these events to trace the progress for debugging purposes.
  */
 export enum ScheduledTaskEvents {
-	/** Event that is emitted if a task piece is not found in the store */
+	/** Event that is emitted if a task piece is not found in the store
+	 * @param task The name of the task what tried to run
+	 * @param payload The payload of the task
+	 */
 	ScheduledTaskNotFound = 'scheduledTaskNotFound',
-	/** Event that is emitted before a task's "run" method is called */
+	/** Event that is emitted before a task's "run" method is called
+	 * @param task The name of the task what tried to run
+	 * @param payload The payload of the task
+	 */
 	ScheduledTaskRun = 'scheduledTaskRun',
-	/** Event that is emitted when a task's "run" method throws an error */
+	/** Event that is emitted when a task's "run" method throws an error
+	 * @param error The error what occurred during the run
+	 * @param task The name of the task what tried to run
+	 * @param duration The duration what indicates how long running the task took
+	 * @param payload The payload of the task
+	 */
 	ScheduledTaskError = 'scheduledTaskError',
-	/** Event that is emitted when a tasks's "run" method is successful */
+	/** Event that is emitted when a tasks's "run" method is successful
+	 * @param task The name of the task what tried to run
+	 * @param payload The payload of the task
+	 * @param result The result of the run
+	 */
 	ScheduledTaskSuccess = 'scheduledTaskSuccess',
-	/** Event that is emitted when a task's "run" method finishes, regardless of whether an error occurred or not */
+	/** Event that is emitted when a task's "run" method finishes, regardless of whether an error occurred or not
+	 * @param task The name of the task what tried to run
+	 * @param duration The duration what indicates how long running the task took
+	 * @param payload The payload of the task
+	 */
 	ScheduledTaskFinished = 'scheduledTaskFinished',
-	/** Event that is emitted when the scheduler fails to connect to the server (i.e. redis or sqs) */
+	/** Event that is emitted when the scheduler fails to connect to the server (i.e. redis or sqs)
+	 * @param error The error occurred the connection
+	 */
 	ScheduledTaskStrategyConnectError = 'scheduledTaskStrategyConnectError'
 }
