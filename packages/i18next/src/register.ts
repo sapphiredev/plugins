138764cc140f74d2f2b3745ed838cac63ea97e4c
Kaname <56084970+kaname-png@users.noreply.github.com>
2022-01-15 12:40:48 -0600

    perf(i18next): "add" event is not necessary in chokidar (#229)

--- packages/i18next/src/register.ts
+++ packages/i18next/src/register.ts
@@ -18,7 +18,7 @@ export class I18nextPlugin extends Plugin {
 			container.logger.info('[i18next-Plugin]: HMR enabled. Watching for languages changes.');
 			const hmr = watch(container.i18n.languagesDirectory, this.options.i18n.hmr.options);
 
-			for (const event of ['add', 'change', 'unlink']) hmr.on(event, () => container.i18n.reloadResources());
+			for (const event of ['change', 'unlink']) hmr.on(event, () => container.i18n.reloadResources());
 		}
 	}
 }
