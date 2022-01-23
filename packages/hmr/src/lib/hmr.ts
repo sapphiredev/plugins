4692d3442c4bf85e1c46cc2c18da0a46f2c3de4f
newt :D <newtykins@outlook.com>
2022-01-23 19:04:18 +0000

    feat(hmr): add silent flag (#233)

--- packages/hmr/src/lib/hmr.ts
+++ packages/hmr/src/lib/hmr.ts
@@ -4,6 +4,7 @@ import { relative } from 'node:path';
 
 export interface HMROptions extends WatchOptions {
 	enabled?: boolean;
+	silent?: boolean;
 }
 
 /**
@@ -16,23 +17,20 @@ export interface HMROptions extends WatchOptions {
  * and if not provided in the object then `enabled` is also set to true.
  *
  */
-export function start({ enabled = true, ...options }: HMROptions = { enabled: true }) {
+export function start({ enabled = true, silent = false, ...options }: HMROptions = { enabled: true }) {
 	// Do not enable plugin when enabled is false
 	if (!enabled) return;
 
-	container.logger.info('[HMR-Plugin]: Enabled. Watching for piece changes.');
+	if (!silent) container.logger.info('[HMR-Plugin]: Enabled. Watching for piece changes.');
 
 	for (const store of container.stores.values()) {
-		const deleteCb = handlePiecePathDelete.bind(null, store);
-		const updateCb = handlePiecePathUpdate.bind(null, store);
-
 		watch([...store.paths], options)
-			.on('change', updateCb)
-			.on('unlink', deleteCb);
+			.on('change', (path) => handlePiecePathUpdate(store, path, silent))
+			.on('unlink', (path) => handlePiecePathDelete(store, path, silent));
 	}
 }
 
-async function handlePiecePathDelete(store: Store<Piece>, path: string) {
+async function handlePiecePathDelete(store: Store<Piece>, path: string, silent: boolean) {
 	if (!store.strategy.filter(path)) return;
 
 	const pieceToDelete = store.find((piece) => piece.location.full === path);
@@ -40,7 +38,7 @@ async function handlePiecePathDelete(store: Store<Piece>, path: string) {
 
 	const result = await fromAsync(async () => {
 		await pieceToDelete.unload();
-		container.logger.info(`[HMR-Plugin]: Unloaded ${pieceToDelete.name} piece from ${pieceToDelete.store.name} store.`);
+		if (!silent) container.logger.info(`[HMR-Plugin]: Unloaded ${pieceToDelete.name} piece from ${pieceToDelete.store.name} store.`);
 	});
 
 	if (isErr(result)) {
@@ -48,7 +46,7 @@ async function handlePiecePathDelete(store: Store<Piece>, path: string) {
 	}
 }
 
-async function handlePiecePathUpdate(store: Store<Piece>, path: string) {
+async function handlePiecePathUpdate(store: Store<Piece>, path: string, silent: boolean) {
 	if (!store.strategy.filter(path)) return;
 
 	const pieceToUpdate = store.find((piece) => piece.location.full === path);
@@ -56,7 +54,7 @@ async function handlePiecePathUpdate(store: Store<Piece>, path: string) {
 	const result = await fromAsync(async () => {
 		if (pieceToUpdate) {
 			await pieceToUpdate.reload();
-			container.logger.info(`[HMR-Plugin]: reloaded ${pieceToUpdate.name} piece from ${pieceToUpdate.store.name} store.`);
+			if (!silent) container.logger.info(`[HMR-Plugin]: reloaded ${pieceToUpdate.name} piece from ${pieceToUpdate.store.name} store.`);
 		} else {
 			const rootPath = [...store.paths].find((storePath) => path.startsWith(storePath));
 			if (!rootPath) throw new Error(`[HMR-Plugin]: Could not find root path for ${path}.`);
@@ -64,9 +62,10 @@ async function handlePiecePathUpdate(store: Store<Piece>, path: string) {
 			const piecesLoaded = await store.load(rootPath, relative(rootPath, path));
 			const piecesLoadedNames = piecesLoaded.map((piece) => piece.name);
 			const piecesLoadedStoreNames = piecesLoaded.map((piece) => piece.store.name);
-			container.logger.info(
-				`[HMR-Plugin]: Loaded ${piecesLoadedNames.join(', ')} piece(s) from ${[...new Set(piecesLoadedStoreNames)].join(', ')} store(s).`
-			);
+			if (!silent)
+				container.logger.info(
+					`[HMR-Plugin]: Loaded ${piecesLoadedNames.join(', ')} piece(s) from ${[...new Set(piecesLoadedStoreNames)].join(', ')} store(s).`
+				);
 		}
 	});
 
