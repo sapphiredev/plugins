1c82587e0e5c4d16539c7ad9220e688543537f72
Jeroen Claassens <jeroen.claassens@live.nl>
2022-05-06 22:08:37 +0200

    Update packages/i18next/src/lib/functions.ts

--- packages/i18next/src/lib/functions.ts
+++ packages/i18next/src/lib/functions.ts
@@ -86,11 +86,7 @@ export async function resolveKey<
 	TKeys extends TFunctionKeys = string,
 	TInterpolationMap extends NonNullObject = StringMap
 >(target: Target, key: TKeys | TKeys[], options?: TOptions<TInterpolationMap>): Promise<TResult> {
-	if (typeof options?.lng === 'string') {
-		return container.i18n.format(options.lng, key, options);
-	}
-
-	return container.i18n.format(await fetchLanguage(target), key, options);
+	return container.i18n.format(typeof options?.lng === 'string' ? options.lng : await fetchLanguage(target), key, options);
 }
 
 /**
