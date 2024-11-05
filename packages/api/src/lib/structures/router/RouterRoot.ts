3e51fea3ba2e0407c3e0ad45f15b148f07ab0836
Aura <kyradiscord@gmail.com>
2024-11-05 09:12:33 +0100

    refactor(route): consider `index` as `/` (#656)

--- packages/api/src/lib/structures/router/RouterRoot.ts
+++ packages/api/src/lib/structures/router/RouterRoot.ts
@@ -51,7 +51,10 @@ export class RouterRoot extends RouterBranch {
 			parts.push(trimmed);
 		}
 
-		parts.push(name.trim());
+		if (name !== 'index') {
+			parts.push(name.trim());
+		}
+
 		return parts.join('/');
 	}
 
