6f07ad843954e3feeea046029a3329f11f55f939
Jeroen Claassens <support@favware.tech>
2025-01-05 14:17:48 +0100

    fix(editable-commands): update PluginMessageUpdate for latest discord.js version

--- packages/editable-commands/src/listeners/PluginMessageUpdate.ts
+++ packages/editable-commands/src/listeners/PluginMessageUpdate.ts
@@ -1,12 +1,12 @@
 import { Events, Listener } from '@sapphire/framework';
-import type { Message } from 'discord.js';
+import type { Message, OmitPartialGroupDMChannel, PartialMessage } from 'discord.js';
 
 export class PluginListener extends Listener<typeof Events.MessageUpdate> {
 	public constructor(context: Listener.LoaderContext) {
 		super(context, { event: Events.MessageUpdate });
 	}
 
-	public override run(old: Message, message: Message) {
+	public override run(old: OmitPartialGroupDMChannel<Message<boolean> | PartialMessage>, message: Message) {
 		// If the contents of both messages are the same, return:
 		if (old.content === message.content) return;
 
